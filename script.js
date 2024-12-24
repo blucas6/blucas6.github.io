c2f70208c888783400d341293d029beb9fc900ee
blucas6 <66273291+blucas6@users.noreply.github.com>
2024-12-23 20:52:50 -0500

    sorted project list

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const user = 'blucas6';
 const getRepos = `https://api.github.com/users/${user}/repos`;
 const projectArea = 'board_area';
 const boxClassName = 'box';
-
 var savedDivs = [];
+const github_auth = {}
 
 // Go through a users github and add each project to the website
 // ----------------------------------------
@@ -14,7 +14,7 @@ async function loadGitHubContent(projectDiv)
     try
     {
         // fetch the repos for the username
-        var ans = await fetch(getRepos);
+        var ans = await fetch(getRepos, github_auth);
         if (ans.status === 403)
         {
             return 403;
@@ -33,7 +33,7 @@ async function loadGitHubContent(projectDiv)
             // fetch info for a repo
             var reponame = repos[i]['name'];
             var url = `https://api.github.com/repos/${user}/${reponame}`;
-            var response = await fetch(url);
+            var response = await fetch(url, github_auth);
             
             // check for errors
             if (response.status === 403)
@@ -44,11 +44,26 @@ async function loadGitHubContent(projectDiv)
             {
                 return response.statusText;
             }
-
+            
             // decode json and add div to site
             var data = await response.json();
-            addProjectDiv(projectDiv, reponame, data['description']);
+            addProjectDiv(reponame, data['description'], data['updated_at']);
         }
+
+        for (let d of savedDivs)
+        {
+            console.log(d.textContent);
+        }
+        // organize list of divs
+        savedDivs.sort((a, b) => {
+            const dateA = new Date(a.querySelector('.last_update').textContent.trim());
+            const dateB = new Date(b.querySelector('.last_update').textContent.trim());
+            return dateB - dateA;
+        });
+
+        // append all of them to the website
+        appendAllSavedRepos(projectDiv);
+
     } catch (error) {
         console.log(error);
         return error;
@@ -56,27 +71,30 @@ async function loadGitHubContent(projectDiv)
     return 0;
 }
 
-// Add a project from github to the website
+// Add a project from github to a list
 // Saves divs in case github can't be reached
 // -----------------------------------------
-// projectDiv: the div to add all projects to
 // repo: the name of the repository for the project
 // desc: a description of the project
-function addProjectDiv(projectDiv, repo, desc)
+// updated_at: last updated date
+function addProjectDiv(repo, desc, updated_at)
 {
     const newDiv = document.createElement('div');
     const header = document.createElement('h4');
     const description = document.createElement('p');
+    const update = document.createElement('p');
     
     header.textContent = repo;
     description.textContent = desc;
+    var date = new Date(updated_at);
+    update.textContent = `${date.toDateString()}`;
+    update.className = 'last_update';
 
     newDiv.appendChild(header);
     newDiv.appendChild(description);
+    newDiv.appendChild(update);
 
     newDiv.className = boxClassName;
-    projectDiv.appendChild(newDiv);
-    
     savedDivs.push(newDiv);
 }
 
@@ -90,11 +108,26 @@ function githubLimitError(projectDiv)
     projectDiv.appendChild(newDiv);
 }
 
+// Appends all div repos from the savedlist to the site
+// ------------------------------------
+// projectDiv: the div to add all projects to
+function appendAllSavedRepos(projectDiv)
+{
+    // append all saved content
+    for (let repoDiv of savedDivs)
+    {
+        projectDiv.appendChild(repoDiv);
+    }
+}
+
 // Execute on load
 window.onload = async function() {
+
     // get the area to populate with github info
     // exit if there is no div to place info
     var projectDiv = document.getElementById(projectArea);
+
+    // make sure we have the div
     if (!projectDiv)
     {
         console.log(`No such thing as ${projectArea}`);
@@ -106,21 +139,10 @@ window.onload = async function() {
         // check for github limit reached
         if (res === 403)
         {
-            // check for saved content
-            if (savedDivs.length > 0)
-            {
-                // append all saved content
-                for (let repoDiv of savedDivs)
-                {
-                    projectDiv.appendChild(repoDiv);
-                }
-            }
-            else
-            {
-                console.log('Github limit error, nothing saved...');
-                // display an the github error
-                githubLimitError(projectDiv);
-            }
+            // TODO: save project info in session storage in case of 403
+            console.log('Github limit error, nothing saved...');
+            // display an the github error
+            githubLimitError(projectDiv);
         }
         // an actual error has occurred
         else if (res === -1)
